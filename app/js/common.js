c41db368eb4e36d63eeae755bf743aa8187c9432
denis <furik1102@gmail.com>
2018-09-24 09:42:33 +0300

    24/09

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,11 @@
 
 $(function () {
 
-
+    $('.menu-link').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('close');
+        $("header .menu-list").slideToggle();
+    });
 
 
 });
